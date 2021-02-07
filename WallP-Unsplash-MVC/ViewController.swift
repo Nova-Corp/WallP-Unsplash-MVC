b877c8237fbeeb9292d6459994211ece8ed6d226
Shanmugarajeshwaran R <shanmugarajeshwaranr@gmail.com>
2021-02-07 10:39:29 +0530

    Initial Commit

---
+++ WallP-Unsplash-MVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WallP-Unsplash-MVC
+//
+//  Created by ADMIN on 07/02/21.
+//  Copyright © 2021 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
